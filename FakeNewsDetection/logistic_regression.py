62c7f437d3cc1349d92b51bcb457f6930659b38f
Octavia Suceava <72067315+917OctaviaSuceava@users.noreply.github.com>
2023-05-28 23:58:18 +0300

    logistic regression

--- FakeNewsDetection/logistic_regression.py
+++ FakeNewsDetection/logistic_regression.py
@@ -1,4 +1,5 @@
 import pickle
+from datetime import time
 
 import pandas as pd
 import nltk
@@ -6,45 +7,100 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import text_preprocessing
+import time
+
 nltk.download('punkt')
 nltk.download('stopwords')
 
-# methods = [text_preprocessing.text_only(), text_preprocessing.text_author(), text_preprocessing.text_title_author()]
+X_pkl = pickle.load(open("tfidf.pickle", "rb"))
+Y_pkl = pickle.load(open("Y1.pickle", "rb"))
 
-def logreg_tfidf():
-    X, Y = text_preprocessing.text_only()
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, stratify=Y, random_state=2)
-    model = LogisticRegression()
-    model.fit(X_train, Y_train)
+X1_pkl = pickle.load(open("tfidf1.pickle", "rb"))
+Y1_pkl = pickle.load(open("Y2.pickle", "rb"))
 
-    train_prediction = model.predict(X_train)
-    accuracy = accuracy_score(train_prediction, Y_train)
-    precision = precision_score(Y_train, train_prediction)
-    recall = recall_score(Y_train, train_prediction)
-    f1 = f1_score(Y_train, train_prediction)
-    print("-------TRAINING DATA RESULTS-------")
-    print("Accuracy:", accuracy)
-    print("Precision:", precision)
-    print("Recall:", recall)
-    print("F1-score:", f1)
+X2_pkl = pickle.load(open("tfidf2.pickle", "rb"))
+Y2_pkl = pickle.load(open("Y3.pickle", "rb"))
 
-    test_prediction = model.predict(X_test)
+X, Y = X_pkl, Y_pkl
+X1, Y1 = X1_pkl, Y1_pkl
+X2, Y2 = X2_pkl, Y2_pkl
+modelLR = LogisticRegression()
+
+
+def logreg_tfidf(x, y):
+    X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.2, stratify=Y, random_state=2)
+    modelLR.fit(X_train, Y_train)
+
+    test_prediction = modelLR.predict(X_test)
     accuracy = accuracy_score(test_prediction, Y_test)
     precision = precision_score(Y_test, test_prediction)
     recall = recall_score(Y_test, test_prediction)
     f1 = f1_score(Y_test, test_prediction)
-    print("-------TEST DATA RESULTS-------")
-    print("Accuracy:", accuracy)
-    print("Precision:", precision)
-    print("Recall:", recall)
-    print("F1-score:", f1)
+
+    return accuracy, precision, recall, f1
 
 
-def logreg_tfidf1():
-    pass
+def train_first():
+    accuracies = []
+    precisions = []
+    recalls = []
+    f1_scores = []
+    print("-----ONLY TEXT-----")
+    for i in range(100):
+        start_time = time.time()
+        accuracy, precision, recall, f1 = logreg_tfidf(X, Y)
+        end_time = time.time()
+        print("finished iteration " + str(i) + "; elapsed time: " + str(end_time - start_time) + "s")
+        accuracies.append(accuracy)
+        precisions.append(precision)
+        recalls.append(recall)
+        f1_scores.append(f1)
+    pickle.dump(modelLR, open("LR1.pickle", "wb"))
+    print("Average Accuracy:", sum(accuracies) / len(accuracies))
+    print("Average Precision:", sum(precisions) / len(precisions))
+    print("Average Recall:", sum(recalls) / len(recalls))
+    print("Average F1-score:", sum(f1_scores) / len(f1_scores))
 
 
-def logreg_tfidf2():
-    pass
+def train_second():
+    accuracies = []
+    precisions = []
+    recalls = []
+    f1_scores = []
+    print("-----TEXT AND AUTHOR-----")
+    for i in range(100):
+        start_time = time.time()
+        accuracy, precision, recall, f1 = logreg_tfidf(X1, Y1)
+        end_time = time.time()
+        print("finished iteration " + str(i) + "; elapsed time: " + str(end_time - start_time) + "s")
+        accuracies.append(accuracy)
+        precisions.append(precision)
+        recalls.append(recall)
+        f1_scores.append(f1)
+    pickle.dump(modelLR, open("LR2.pickle", "wb"))
+    print("Average Accuracy:", sum(accuracies) / len(accuracies))
+    print("Average Precision:", sum(precisions) / len(precisions))
+    print("Average Recall:", sum(recalls) / len(recalls))
+    print("Average F1-score:", sum(f1_scores) / len(f1_scores))
 
-logreg_tfidf()
+
+def train_third():
+    accuracies = []
+    precisions = []
+    recalls = []
+    f1_scores = []
+    print("-----TEXT, TITLE AND AUTHOR-----")
+    for i in range(100):
+        start_time = time.time()
+        accuracy, precision, recall, f1 = logreg_tfidf(X2, Y2)
+        end_time = time.time()
+        print("finished iteration " + str(i) + "; elapsed time: " + str(end_time - start_time) + "s")
+        accuracies.append(accuracy)
+        precisions.append(precision)
+        recalls.append(recall)
+        f1_scores.append(f1)
+    pickle.dump(modelLR, open("LR2.pickle", "wb"))
+    print("Average Accuracy:", sum(accuracies) / len(accuracies))
+    print("Average Precision:", sum(precisions) / len(precisions))
+    print("Average Recall:", sum(recalls) / len(recalls))
+    print("Average F1-score:", sum(f1_scores) / len(f1_scores))
--- FakeNewsDetection/text_preprocessing.py
+++ FakeNewsDetection/text_preprocessing.py
@@ -6,8 +6,12 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
+
 nltk.download('punkt')
 nltk.download('stopwords')
+Y = None
+Y1 = None
+Y2 = None
 
 
 def preprocess_text(text):
@@ -32,6 +36,7 @@ def preprocess_text(text):
 
 
 def text_only():
+    global Y
     pd.set_option('display.max_columns', None)
     data = pd.read_csv('data/train.csv', na_filter=False)
     print(data.head())
@@ -49,11 +54,13 @@ def text_only():
 
     pickle.dump(tf_idf_matrix, open("tfidf.pickle", "wb"))
     Y = data.label.values
+    pickle.dump(Y, open("D:\\ThesisProject\\FakeNewsDetection\\Y1.pickle", "wb"))
     print(Y)
-    return tf_idf_matrix, Y
+    # return tf_idf_matrix, Y
 
 
 def text_author():
+    global Y1
     pd.set_option('display.max_columns', None)
     data = pd.read_csv('data/train.csv', na_filter=False)
     print(data.head())
@@ -71,9 +78,12 @@ def text_author():
     tf_idf_matrix = tfidf_vectorizer.transform(preprocessed_data.dropna())
 
     pickle.dump(tf_idf_matrix, open("tfidf1.pickle", "wb"))
+    Y1 = data.label.values
+    pickle.dump(Y1, open("D:\\ThesisProject\\FakeNewsDetection\\Y2.pickle", "wb"))
 
 
 def text_title_author():
+    global Y2
     pd.set_option('display.max_columns', None)
     data = pd.read_csv('data/train.csv', na_filter=False)
     print(data.head())
@@ -94,6 +104,10 @@ def text_title_author():
     tf_idf_matrix = tfidf_vectorizer.transform(preprocessed_data.dropna())
 
     pickle.dump(tf_idf_matrix, open("tfidf2.pickle", "wb"))
+    Y2 = data.label.values
+    pickle.dump(Y2, open("D:\\ThesisProject\\FakeNewsDetection\\Y3.pickle", "wb"))
 
 
 text_only()
+text_author()
+text_title_author()
